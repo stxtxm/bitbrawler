ca3f54f59f5a53475a22d60e435f49b4419608c5
Timo <sitizstorm@gmail.com>
2026-06-13 19:13:01 +0200

    chore: bump service worker version to v4 to force cache refresh for returning users

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v3'
+const VERSION = 'v4'
 const APP_SHELL_CACHE = `bitbrawler-shell-${VERSION}`
 const ASSET_CACHE = `bitbrawler-assets-${VERSION}`
 const RUNTIME_CACHE = `bitbrawler-runtime-${VERSION}`
